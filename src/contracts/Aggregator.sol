caa575680a424cf7f1e94070b628a2d3b7a327e0
Justin <48664609+GlitchicaL@users.noreply.github.com>
2021-09-19 10:59:13 -0400

    Fetch & Compare Exchange Rates

--- src/contracts/Aggregator.sol
+++ src/contracts/Aggregator.sol
@@ -1,13 +1,100 @@
 pragma solidity ^0.5.16;
 
+// Interface for ERC20 DAI contract
+interface DAI {
+    function approve(address, uint256) external returns (bool);
+
+    function transfer(address, uint256) external returns (bool);
+}
+
+// Interface for Compound's cDAI contract
+interface cDAI {
+    function mint(uint256) external returns (uint256);
+
+    function exchangeRateCurrent() external returns (uint256);
+}
+
+// Interface for Aave's lending pool contract
+interface AaveLendingPool {
+    function getReserveData(address asset)
+        external
+        returns (
+            uint256 configuration,
+            uint128 liquidityIndex,
+            uint128 variableBorrowIndex,
+            uint128 currentLiquidityRate,
+            uint128 currentVariableBorrowRate,
+            uint128 currentStableBorrowRate,
+            uint40 lastUpdateTimestamp,
+            address aTokenAddress,
+            address stableDebtTokenAddress,
+            address variableDebtTokenAddress,
+            address interestRateStrategyAddress,
+            uint8 id
+        );
+}
+
 contract Aggregator {
     // Variables
     string public test = "Contract Smoke Test";
 
     // Events
+    event Deposit(string selectedExchange);
 
     // Constructor
     constructor() public {}
 
     // Functions
+
+    function deposit(
+        address _DAI,
+        address _cDAI,
+        address _AaveLendingPool
+    ) public {
+        // Instiantiate contracts
+        DAI dai = DAI(_DAI);
+        cDAI cDai = cDAI(_cDAI);
+        AaveLendingPool lendingPool = AaveLendingPool(_AaveLendingPool);
+
+        string memory selectedExchange;
+
+        // Fetch interest rates
+        uint256 compoundRate = getCompoundExchangeRate(_cDAI);
+        uint256 aaveRate = getAaveExchangeRate(_AaveLendingPool, _DAI);
+
+        // Compare interest rates
+        if (compoundRate > aaveRate) {
+            // Deposit into Compound
+            selectedExchange = "Compound";
+        } else {
+            // Deposit into Aave
+            selectedExchange = "Aave";
+        }
+
+        // Emit Deposit event
+        emit Deposit(selectedExchange);
+    }
+
+    // ---
+
+    // Get Compound's interest rate
+    function getCompoundExchangeRate(address _cDAI) public returns (uint256) {
+        cDAI token = cDAI(_cDAI);
+        uint256 exchangeRate = (token.exchangeRateCurrent() / 10); // Fetch exchange rate
+
+        return exchangeRate;
+    }
+
+    // Get Aave's interest rate
+    function getAaveExchangeRate(address _AaveLendingPool, address _DAI)
+        public
+        returns (uint128)
+    {
+        AaveLendingPool lendingPool = AaveLendingPool(_AaveLendingPool);
+
+        (, , , uint128 currentLiquidityRate, , , , , , , , ) = lendingPool
+            .getReserveData(_DAI);
+
+        return currentLiquidityRate;
+    }
 }
--- test/Aggregator.test.js
+++ test/Aggregator.test.js
@@ -4,8 +4,14 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Aggregator', () => {
+contract('Aggregator', ([deployer]) => {
     const test = "Contract Smoke Test"
+
+    const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // ERC20 DAI Address
+    const cDAI = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643' // Compound's cDAI Address
+    const aDAI = '0x028171bCA77440897B824Ca71D1c56caC55b68A3' // Aave's aDAI Address
+    let aaveLendingPool = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9' // Aave's Lending Pool Contract 
+
     let aggregator
 
     beforeEach(async () => {
@@ -19,5 +25,32 @@ contract('Aggregator', () => {
             const result = await aggregator.test()
             result.should.equal(test)
         })
+    })
+
+    describe('exchange rates', async () => {
+
+        it('fetches compound exchange rate', async () => {
+            const result = await aggregator.getCompoundExchangeRate.call(cDAI)
+            console.log(result.toString())
+            result.should.not.equal(0)
+        })
+
+        it('fetches aave exchange rate', async () => {
+            const result = await aggregator.getAaveExchangeRate.call(aaveLendingPool, DAI)
+            console.log(result.toString())
+            result.should.not.equal(0)
+        })
+    })
+
+    describe('deposits', async () => {
+
+        it('emits deposit event', async () => {
+            const result = await aggregator.deposit(DAI, cDAI, aaveLendingPool)
+            console.log(result.logs[0].args.selectedExchange)
+
+            const log = result.logs[0]
+            log.event.should.equal('Deposit')
+        })
+
     })
 })
