ed57fa3cbbe1cecf3937325946250e486be9b0f5
Justin <48664609+GlitchicaL@users.noreply.github.com>
2021-09-21 10:47:57 -0400

    Deposit & Withdraw from Compound

--- src/contracts/Aggregator.sol
+++ src/contracts/Aggregator.sol
@@ -1,21 +1,45 @@
 pragma solidity ^0.5.16;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 // Interface for ERC20 DAI contract
 interface DAI {
     function approve(address, uint256) external returns (bool);
 
     function transfer(address, uint256) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
 }
 
 // Interface for Compound's cDAI contract
 interface cDAI {
     function mint(uint256) external returns (uint256);
 
+    function redeemUnderlying(uint256) external returns (uint256);
+
     function exchangeRateCurrent() external returns (uint256);
 }
 
 // Interface for Aave's lending pool contract
 interface AaveLendingPool {
+    function deposit(
+        address asset,
+        uint256 amount,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external;
+
+    function safeDeposit(
+        address asset,
+        uint256 amount,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external;
+
     function getReserveData(address asset)
         external
         returns (
@@ -35,11 +59,16 @@ interface AaveLendingPool {
 }
 
 contract Aggregator {
+    using SafeMath for uint256;
+
     // Variables
     string public test = "Contract Smoke Test";
+    mapping(address => uint256) balances; // Keep track of user balance
+    mapping(address => address) locations; // Keep track of where the user balance is stored
 
     // Events
-    event Deposit(string selectedExchange);
+    event Deposit(address owner, uint256 amount, address depositTo);
+    event Withdraw(address owner, uint256 amount, address withdrawFrom);
 
     // Constructor
     constructor() public {}
@@ -49,30 +78,83 @@ contract Aggregator {
     function deposit(
         address _DAI,
         address _cDAI,
-        address _AaveLendingPool
+        address _AaveLendingPool,
+        uint256 _amount
     ) public {
+        require(_amount > 0);
+
         // Instiantiate contracts
         DAI dai = DAI(_DAI);
-        cDAI cDai = cDAI(_cDAI);
-        AaveLendingPool lendingPool = AaveLendingPool(_AaveLendingPool);
 
-        string memory selectedExchange;
+        dai.transferFrom(msg.sender, address(this), _amount);
+        balances[msg.sender] = balances[msg.sender].add(_amount);
 
         // Fetch interest rates
         uint256 compoundRate = getCompoundExchangeRate(_cDAI);
         uint256 aaveRate = getAaveExchangeRate(_AaveLendingPool, _DAI);
 
         // Compare interest rates
-        if (compoundRate > aaveRate) {
+        if (true) {
             // Deposit into Compound
-            selectedExchange = "Compound";
+            require(_depositToCompound(_DAI, _cDAI, _amount) == 0);
+
+            // Update location
+            locations[msg.sender] = _cDAI;
         } else {
             // Deposit into Aave
-            selectedExchange = "Aave";
         }
 
         // Emit Deposit event
-        emit Deposit(selectedExchange);
+        emit Deposit(msg.sender, _amount, locations[msg.sender]);
+    }
+
+    function withdraw(
+        address _DAI,
+        address _cDAI,
+        address _AaveLendingPool
+    ) public {
+        require(balances[msg.sender] > 0);
+
+        // Instiantiate contracts
+        DAI dai = DAI(_DAI);
+
+        // Determine where the user funds are stored
+        if (locations[msg.sender] == _cDAI) {
+            require(_withdrawFromCompound(_cDAI) == 0);
+        } else {
+            // Withdraw from Aave
+        }
+
+        // Once we have the funds, transfer back to owner
+        dai.transfer(msg.sender, balances[msg.sender]);
+
+        emit Withdraw(msg.sender, balances[msg.sender], locations[msg.sender]);
+
+        // Reset user balance
+        balances[msg.sender] = 0;
+    }
+
+    function _depositToCompound(
+        address _DAI,
+        address _cDAI,
+        uint256 _amount
+    ) internal returns (uint256) {
+        // Instiantiate contracts
+        DAI dai = DAI(_DAI);
+        cDAI cDai = cDAI(_cDAI);
+
+        require(dai.approve(address(cDai), _amount));
+
+        uint256 result = cDai.mint(_amount);
+        return result;
+    }
+
+    function _withdrawFromCompound(address _cDAI) internal returns (uint256) {
+        // Instiantiate contract & redeem
+        cDAI cDai = cDAI(_cDAI);
+
+        uint256 result = cDai.redeemUnderlying(balances[msg.sender]);
+        return result;
     }
 
     // ---
@@ -96,5 +178,14 @@ contract Aggregator {
             .getReserveData(_DAI);
 
         return currentLiquidityRate;
+    }
+
+    // ---
+    function balanceOf() public view returns (uint256) {
+        return balances[msg.sender];
+    }
+
+    function balanceWhere() public view returns (address) {
+        return locations[msg.sender];
     }
 }
--- test/Aggregator.test.js
+++ test/Aggregator.test.js
@@ -1,17 +1,21 @@
-const Aggregator = artifacts.require("./Aggregator");
+const Aggregator = artifacts.require("./Aggregator")
+const daiABI = require("../mint-dai/dai-abi.json")
 
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
+const DAI = '0x6b175474e89094c44da98b954eedeac495271d0f' // ERC20 DAI Address
+const cDAI = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643' // Compound's cDAI Address
+const aDAI = '0x028171bCA77440897B824Ca71D1c56caC55b68A3' // Aave's aDAI Address
+const aaveLendingPool = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9' // Aave's Lending Pool Contract
+
+const EVM_REVERT = 'VM Exception while processing transaction: revert'
+
 contract('Aggregator', ([deployer]) => {
     const test = "Contract Smoke Test"
 
-    const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // ERC20 DAI Address
-    const cDAI = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643' // Compound's cDAI Address
-    const aDAI = '0x028171bCA77440897B824Ca71D1c56caC55b68A3' // Aave's aDAI Address
-    let aaveLendingPool = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9' // Aave's Lending Pool Contract 
-
+    const daiContract = new web3.eth.Contract(daiABI, DAI);
     let aggregator
 
     beforeEach(async () => {
@@ -44,13 +48,84 @@ contract('Aggregator', ([deployer]) => {
 
     describe('deposits', async () => {
 
-        it('emits deposit event', async () => {
-            const result = await aggregator.deposit(DAI, cDAI, aaveLendingPool)
-            console.log(result.logs[0].args.selectedExchange)
+        let amount = 10
+        let amountInWei = web3.utils.toWei(amount.toString(), 'ether')
+        let result
 
-            const log = result.logs[0]
-            log.event.should.equal('Deposit')
+        describe('success', async () => {
+            beforeEach(async () => {
+                // Approve
+                await daiContract.methods.approve(aggregator.address, amountInWei).send({ from: deployer })
+
+                // Initiate deposit
+                result = await aggregator.deposit(DAI, cDAI, aaveLendingPool, amountInWei, { from: deployer })
+            })
+
+            it('tracks the dai amount', async () => {
+                // Check dai balance in smart contract
+                let balance
+                balance = await aggregator.balanceOf.call({ from: deployer })
+                console.log(balance.toString())
+                balance.toString().should.equal(amountInWei.toString())
+            })
+
+            it('tracks where dai is stored', async () => {
+                result = await aggregator.balanceWhere.call({ from: deployer })
+                console.log(result)
+            })
+
+            it('emits deposit event', async () => {
+                const log = result.logs[0]
+                log.event.should.equal('Deposit')
+            })
+        })
+
+        describe('failure', async () => {
+
+            it('fails when transfer is not approved', async () => {
+                await aggregator.deposit(DAI, cDAI, aaveLendingPool, amountInWei, { from: deployer }).should.be.rejectedWith(EVM_REVERT)
+            })
+
+            it('fails when amount is 0', async () => {
+                await aggregator.deposit(DAI, cDAI, aaveLendingPool, 0, { from: deployer }).should.be.rejectedWith(EVM_REVERT)
+            })
+
         })
 
     })
+
+    describe('withdraws', async () => {
+
+        let amount = 10
+        let amountInWei = web3.utils.toWei(amount.toString(), 'ether')
+        let result
+
+        describe('success', async () => {
+            beforeEach(async () => {
+                // Approve
+                await daiContract.methods.approve(aggregator.address, amountInWei).send({ from: deployer })
+
+                // Initiate deposit
+                await aggregator.deposit(DAI, cDAI, aaveLendingPool, amountInWei, { from: deployer })
+            })
+
+            it('emits withdraw event', async () => {
+                result = await aggregator.withdraw(DAI, cDAI, aaveLendingPool, { from: deployer })
+                const log = result.logs[0]
+                log.event.should.equal('Withdraw')
+            })
+
+        })
+
+        describe('failure', async () => {
+
+            it('fails if user has no balance', async () => {
+                await aggregator.withdraw(DAI, cDAI, aaveLendingPool, { from: deployer }).should.be.rejectedWith(EVM_REVERT)
+            })
+
+        })
+
+    })
+
+
 })
