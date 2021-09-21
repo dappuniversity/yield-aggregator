871a8fc9155fd4fb7d6048ff60eb36db3c5490eb
Justin <48664609+GlitchicaL@users.noreply.github.com>
2021-09-21 13:59:31 -0400

    Updated Frontend Styles

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 			<div>
 				<nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
 					<a
-						className="navbar-brand col-sm-3 col-md-2 mr-0"
+						className="navbar-brand col-sm-3 col-md-2 mr-0 mx-3"
 						href="http://www.dappuniversity.com/bootcamp"
 						target="_blank"
 						rel="noopener noreferrer"
@@ -16,10 +16,11 @@ class App extends Component {
 						Dapp University
 					</a>
 				</nav>
-				<div className="container-fluid mt-5">
-					<div className="row">
-						<main role="main" className="col-lg-12 d-flex text-center">
-							<div className="content mr-auto ml-auto">
+				<div className="container-fluid">
+					<main role="main" className="col-lg-12 text-center">
+						<div className="row">
+							<div className="col">
+								<h1 className="my-5">Yield Aggregator</h1>
 								<a
 									href="http://www.dappuniversity.com/bootcamp"
 									target="_blank"
@@ -27,21 +28,22 @@ class App extends Component {
 								>
 									<img src={logo} className="App-logo" alt="logo" />
 								</a>
-								<h1>Dapp University Starter Kit</h1>
-								<p>
-									Edit <code>src/components/App.js</code> and save to reload.
-								</p>
-								<a
-									className="App-link"
-									href="http://www.dappuniversity.com/bootcamp"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-								</a>
 							</div>
-						</main>
-					</div>
+						</div>
+						<div className="row content">
+							<div className="col user-controls">
+								<button>Deposit</button>
+								<button>Rebalance</button>
+								<button>Withdraw</button>
+							</div>
+							<div className="col user-stats">
+								<p>Current Wallet Balance (DAI): 0</p>
+								<p>Aggregator Balance (DAI): 0</p>
+								<p>Active Protocol: UNDEFINED</p>
+							</div>
+						</div>
+					</main>
+
 				</div>
 			</div>
 		);
