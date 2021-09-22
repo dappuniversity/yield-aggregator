d639df449d25d8b671feda63d87183a067adc3b8
Justin <48664609+GlitchicaL@users.noreply.github.com>
2021-09-22 11:26:02 -0400

    Allow User Deposits

--- src/components/App.js
+++ src/components/App.js
@@ -18,14 +18,18 @@ class App extends Component {
 			web3: null,
 			aggregator: null,
 			dai: null,
+			cDAI: "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643", // Address of Compound's cDAI
+			aaveLendingPool: "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9", // Address of aaveLendingPool
 			account: "0x0",
 			walletBalance: "0",
 			aggregatorBalance: "0",
 			activeProtocol: "None",
+			amountToDeposit: "0",
 			loading: true
 		};
 
 		// Binding methods here
+		this.depositHandler = this.depositHandler.bind(this);
 
 	}
 
@@ -49,8 +53,6 @@ class App extends Component {
 	}
 
 	async loadBlockchainData(dispatch) {
-		// await window.ethereum.enable();
-
 		const web3 = new Web3(window.ethereum)
 		this.setState({ web3 })
 
@@ -89,9 +91,40 @@ class App extends Component {
 		this.setState({ walletBalance })
 		this.setState({ aggregatorBalance })
 
-		if (activeProtocol !== "0x0000000000000000000000000000000000000000") {
-			this.setState({ activeProtocol })
+		if (activeProtocol === this.state.cDAI) {
+			this.setState({ activeProtocol: "Compound" })
+		} else if (activeProtocol === this.state.aaveLendingPool) {
+			this.setState({ activeProtocol: "Aave" })
+		} else {
+			this.setState({ activeProtocol: "None" })
 		}
+	}
+
+	async depositHandler() {
+		if (this.state.walletBalance == 0) {
+			window.alert('No funds in wallet')
+			return
+		}
+
+		if (this.state.amountToDeposit <= 0) {
+			window.alert('Cannot be 0 or negative')
+			return
+		}
+
+		const amount = this.state.web3.utils.toWei(this.state.amountToDeposit.toString(), 'ether')
+
+		this.state.dai.methods.approve(this.state.aggregator._address, amount).send({ from: this.state.account })
+			.on('transactionHash', () => {
+				this.state.aggregator.methods.deposit(
+					this.state.dai._address,
+					this.state.cDAI,
+					this.state.aaveLendingPool,
+					amount
+				).send({ from: this.state.account })
+					.on('transactionHash', () => {
+						this.loadAccountInfo()
+					})
+			})
 	}
 
 	render() {
@@ -114,7 +147,13 @@ class App extends Component {
 						</div>
 						<div className="row content">
 							<div className="col user-controls">
-								<button>Deposit</button>
+								<form onSubmit={(e) => {
+									e.preventDefault()
+									this.depositHandler()
+								}}>
+									<input type="number" placeholder="Amount" onChange={(e) => this.setState({ amountToDeposit: e.target.value })} />
+									<button type="submit">Deposit</button>
+								</form>
 								<button>Rebalance</button>
 								<button>Withdraw</button>
 							</div>
